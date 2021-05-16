cab8ac22902fe1718fb062deafbda7566d5f4df3
Andre Pierre <andre.laurence94@gmail.com>
2021-05-16 19:37:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 const append = ["Milo", "Otis", "Garfield"]; 
 append.push("Odie")
 
-const prepend = ["Milo", "Otis", "Garfield"].shift("Odie")
+const prepend = ["Milo", "Otis", "Garfield"]
+prepend.unshift("Odie")
